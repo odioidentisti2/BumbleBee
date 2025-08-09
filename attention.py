Fixed depict (now uses attn_weights after softmax)

attention.py
@@ -86,8 +86,7 @@ class MultiHeadAttention(nn.Module):
         out = out.transpose(1, 2).reshape(batch_size, -1, self.num_heads * head_dim)
         # Final output projection with a residual connection and nonlinearity (Mish)
         out = out + F.mish(self.fc_o(out))
-        attn_scores = attn_scores.mean(dim=1)  # Averaging scores across heads
-        return out, attn_scores
+        return out, attn_weights.mean(dim=1)  # Averaging attention across heads (I SHOULD INSPECT fc_o WEIGHTS INSTEAD)
 
 # Same input for both Q and K
 class SelfAttention(nn.Module):

model_explainer.py
@@ -7,33 +7,18 @@ import io
 
 
 def depict(data, attention, edge_index):
-    """
-    Visualize molecule with attention-based highlighting in a popup window
-    
-    Args:
-        mol: RDKit molecule object
-        graph_attention: tensor of attention weights per edge [num_edges]
-        graph_edge_index: edge connectivity [2, num_edges]
-    """
     data = data.to('cpu')  # Ensure data is on CPU for RDKit
     print(int(data.y.item()), data.smiles)
+    
     if (mol := data.mol) is None:
         print("Invalid molecule object")
         return None
     
-    # Convert attention weights to numpy
-    if torch.is_tensor(attention):
-        weights = attention.detach().cpu().numpy()
-    else:
-        weights = attention
+    weights = attention.detach().cpu().numpy()
+    edge_index = edge_index.detach().cpu().numpy()
     
-    # Convert edge_index to numpy if needed
-    if torch.is_tensor(edge_index):
-        edge_index = edge_index.detach().cpu().numpy()
-    else:
-        edge_index = edge_index
-    
-    # Get bond indices and atoms for highlighting
+    bond_intensity = {}
+    atom_intensity = {}
     highlight_bonds = []
     highlight_atoms = set()
     bond_colors = {}
@@ -43,29 +28,35 @@ def depict(data, attention, edge_index):
     # threshold = 1 / len(weights)  # to be used only after softmax
     threshold = weights.mean() + weights.std()  # Mean + 1 std dev
     
-    # Map edge_index to RDKit bond indices
+    # Bond intensity
     for i, (src, dst) in enumerate(edge_index.T):
         src_idx, dst_idx = int(src), int(dst)
-        
-        # Find corresponding bond in RDKit molecule
-        bond = mol.GetBondBetweenAtoms(src_idx, dst_idx)
-        if bond is not None:
-            bond_idx = bond.GetIdx()
-            weight = weights[i]
-            # bond_labels[bond_idx] = f"{weight:.3f}" 
-            
-            if weight > threshold:  # Only highlight important edges
-                highlight_bonds.append(bond_idx)
-                highlight_atoms.add(src_idx)
-                highlight_atoms.add(dst_idx)
-                
-                # Color intensity based on attention (red scale)
-                # intensity = float(min(weight / weights.max(), 1.0))
-                intensity = float(((weight / weights.max()) / 2) + 0.5)
-                bond_colors[bond_idx] = (1.0, 1.0-intensity, 1.0-intensity)
-                atom_colors[src_idx] = (1.0, 1.0-intensity*0.5, 1.0-intensity*0.5)
-                atom_colors[dst_idx] = (1.0, 1.0-intensity*0.5, 1.0-intensity*0.5)
-                # print(bond_idx, intensity)
+        bond_idx = mol.GetBondBetweenAtoms(src_idx, dst_idx).GetIdx()
+        weight = weights[i]
+        if bond_intensity.get(bond_idx) is None:
+            bond_intensity[bond_idx] = weight
+        else:
+            bond_intensity[bond_idx] = max(bond_intensity[bond_idx], weight)  # Keep max weight for bidirectional bonds
+    
+    atom_norm_intensity = {}
+    for bond_idx, weight in bond_intensity.items():
+        bond = mol.GetBondWithIdx(bond_idx)
+        bond.SetProp("bondNote", str(bond_idx))  # Draw bond index
+        if weight > threshold:  # Only highlight important bonds
+            highlight_bonds.append(bond_idx)
+            norm_intensity = float((weight / max(bond_intensity.values())))
+            bond_colors[bond_idx] = (1.0, 1.0-norm_intensity, 1.0-norm_intensity)
+            # Atoms connected by this bond
+            for atom in (bond.GetBeginAtom(), bond.GetEndAtom()):
+                atom_idx = atom.GetIdx()
+                if atom_norm_intensity.get(atom_idx) is None:
+                    atom_norm_intensity[atom_idx] = norm_intensity
+                else:
+                    atom_norm_intensity[atom_idx] = max(atom_norm_intensity[atom_idx], norm_intensity)
+                atom_colors[atom_idx] = (1.0,
+                                         1.0 - atom_norm_intensity[atom_idx], 
+                                         1.0 - atom_norm_intensity[atom_idx])
+                highlight_atoms.add(atom_idx)
     
     # Create drawer
     drawer = rdMolDraw2D.MolDraw2DCairo(500, 500)
@@ -78,26 +69,26 @@ def depict(data, attention, edge_index):
     opts.annotationFontScale = 0.8  # Adjust font size for bond labels
     opts.prepareMolsBeforeDrawing = False  # Important for custom drawing
 
-    for bond in mol.GetBonds():
-        bond.SetProp("bond_idx", str(bond.GetIdx()))  # Set bond annotation
+    target_label = 'Toxic' if data.y.item() == 1 else 'Non-toxic'
+    legend = f"{data.smiles}\n{target_label}"
 
     # Draw molecule with highlighting
     if highlight_bonds or highlight_atoms:
-        drawer.DrawMolecule(mol, 
+        drawer.DrawMolecule(mol,
                            highlightAtoms=list(highlight_atoms),
                            highlightAtomColors=atom_colors,
                            highlightBonds=highlight_bonds,
-                           highlightBondColors=bond_colors)
+                           highlightBondColors=bond_colors,
+                           legend=legend) 
     else:
         # No highlighting if no important edges found
-        drawer.DrawMolecule(mol, data.smiles)
-    drawer.DrawString(data.smiles, Point2D(10, 480))  # Position at bottom-left
-    
+        drawer.DrawMolecule(mol, legend=legend) 
+        
     drawer.FinishDrawing()
     
     # Get image data and display in popup
     img_data = drawer.GetDrawingText()
-    
+
     # Convert to PIL Image
     img = Image.open(io.BytesIO(img_data))
     
@@ -107,6 +98,8 @@ def depict(data, attention, edge_index):
     print(f"Displayed molecule with {len(highlight_bonds)} highlighted bonds and {len(highlight_atoms)} highlighted atoms")
     print(f"Weights range: {attention.min():.3f} - {attention.max():.3f}")
     print(attention)
+    for i, norm_intensity in bond_intensity.items():
+        print(f"Bond {i}: {norm_intensity:.3f}")
     input("Press Enter to continue...")
 
     return img_data