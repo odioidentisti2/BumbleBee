f6c673ac619c21c8626046330cdd436c5ec7ad5c
totem <odioidentisti@gmail.com>
2025-11-13 15:09:49 +0100

    cross-validation

--- bumblebee.py
+++ bumblebee.py
@@ -1,98 +1,13 @@
 import time
 import torch
-import torch.nn as nn
 from torch_geometric.loader import DataLoader
-from torch_geometric.utils import to_dense_batch
-from architectures import ESA, mlp
 from molecular_data import GraphDataset, ATOM_DIM, BOND_DIM
-from adj_mask_utils import edge_adjacency, edge_mask
+from model import MAGClassifier
 from explainer import *
 
 
-class MAGClassifier(nn.Module):
-    
-    def __init__(self, node_dim, edge_dim, hidden_dim=128, mlp_hidden_dim=128, num_heads=8, output_dim=1):
-        super(MAGClassifier, self).__init__()
-        self.hidden_dim = hidden_dim
-        # Edge feature encoder (node-edge MLP)
-        self.input_mlp = mlp(2 * node_dim + edge_dim, mlp_hidden_dim, hidden_dim)
-        # ESA block
-        self.esa = ESA(hidden_dim, num_heads, 'MMSP')
-        # Classifier
-        self.output_mlp = mlp(hidden_dim, mlp_hidden_dim, output_dim)
-
-    def batch_forward(self, edge_features, edge_index, node_batch):
-        batched_h = self.input_mlp(edge_features)  # [batch_edges, hidden_dim]
-        edge_batch = self._edge_batch(edge_index, node_batch)  # [batch_edges]
-        max_edges = torch.bincount(edge_batch).max().item()
-        dense_batch_h, pad_mask = to_dense_batch(batched_h, edge_batch, fill_value=0, max_num_nodes=max_edges)
-        batch_size = node_batch.max().item() + 1
-        adj_mask = edge_mask(edge_index, node_batch, batch_size, max_edges)
-        out = self.esa(dense_batch_h, adj_mask, pad_mask)  # [batch_size, hidden_dim]
-        # out = torch.where(pad_mask.unsqueeze(-1), out, torch.zeros_like(out))
-        logits = self.output_mlp(out)    # [batch_size, output_dim]
-        return torch.flatten(logits)     # [batch_size] 
-
-    def single_forward(self, edge_features, edge_index, node_batch, return_attention=False):
-        self.esa.expose_attention(return_attention)
-        batched_h = self.input_mlp(edge_features)  # [batch_edges, hidden_dim]
-        edge_batch = self._edge_batch(edge_index, node_batch)  # [batch_edges]
-        batch_size = node_batch.max().item() + 1
-        src, dst = edge_index
-        attn_weights = []
-        out = torch.zeros((batch_size, self.hidden_dim), device=edge_features.device)
-        for i in range(batch_size):
-            graph_mask = (edge_batch == i)
-            h = batched_h[graph_mask]  # [graph_edges, hidden_dim]
-            adj_mask = edge_adjacency(src[graph_mask], dst[graph_mask])  # [graph_edges, graph_edges]
-            h = h.unsqueeze(0)  # Add batch dimension
-            adj_mask = adj_mask.unsqueeze(0)  # Add batch dimension
-            out[i] = self.esa(h, adj_mask)  # [hidden_dim]
-            # out[i] = out[i].squeeze(0)  # Remove batch dimension ???
-            if return_attention:
-                attn = self.esa.get_attention().squeeze(0)  # Remove batch dimension
-                attn_weights.append(attn.detach().cpu())
-                # if i == batch_size - 1:
-                #     return attn_weights
-        if return_attention:
-            return attn_weights
-        logits = self.output_mlp(out)    # [batch_size, output_dim]
-        return torch.flatten(logits)     # [batch_size]
-    
-    def forward(self, batch):
-        """
-        Args:
-            batch: batch from DataLoader (torch_geometric.data.Batch)
-                batch.x              # [batch_nodes, node_dim]
-                batch.edge_index     # [2, batch_edges]
-                batch.edge_attr      # [batch_edges, edge_dim]
-                batch.batch          # [batch_nodes]
-        """
-        edge_feat = MAGClassifier.get_features(batch)
-
-        if BATCH_DEBUG or edge_feat.device.type == 'cuda':  # GPU: batch Attention
-            return self.batch_forward(edge_feat, batch.edge_index, batch.batch)
-        else:  # per-graph Attention (faster on CPU)
-            return self.single_forward(edge_feat, batch.edge_index, batch.batch)
-        # if not torch.allclose(batch_logits, single_logits, rtol=1e-4, atol=1e-7):
-        #     print("WARNING: Batch and Single logits differ!")
-        # return batch_logits
-        
-    @staticmethod
-    def get_features(batch):
-        # Concatenate node (src and dst) and edge features
-        src, dst = batch.edge_index
-        return torch.cat([batch.x[src], batch.x[dst], batch.edge_attr], dim=1)
-
-    @staticmethod
-    def _edge_batch(edge_index, node_batch):
-        # Given edge_index [2, num_edges] and node_batch [num_nodes], 
-        # return edge_batch [num_edges] where each edge takes the batch idx of its src node
-        # (assumes all edges within a graph)
-        return node_batch[edge_index[0]]
-
-# trainer.fit
-def train(model, loader, optimizer, criterion, epoch):
+# Trainer.fit from pytorch_lightning does the same job
+def train(model, loader, optimizer, criterion):
     model.train()  # set training mode
     total_loss = 0
     total = 0
@@ -100,11 +15,11 @@ def train(model, loader, optimizer, criterion, epoch):
         batch = batch.to(DEVICE)
         targets = batch.y.view(-1).to(DEVICE)
         optimizer.zero_grad()  # zero gradients
-        logits = model(batch)  # forward pass
+        logits = model(batch, BATCH_DEBUG)  # forward pass
         loss = criterion(logits, targets)  # calculate loss
         loss.backward()  # backward pass
         optimizer.step()  # update weights
-        # calculate statistics
+        # statistics
         total_loss += loss.item() * batch.num_graphs
         total += batch.num_graphs
     return total_loss / total
@@ -118,12 +33,12 @@ def test(model, loader, criterion):
         batch = batch.to(DEVICE)
         targets = batch.y.view(-1).to(DEVICE)
         with torch.no_grad():
-            logits = model(batch)  # forward pass
+            logits = model(batch, BATCH_DEBUG)  # forward pass
             loss = criterion(logits, targets)
             total_loss += loss.item() * batch.num_graphs
+            total += batch.num_graphs
             preds = (torch.sigmoid(logits) > 0.5).float()
             correct += (preds == targets).sum().item()
-            total += batch.num_graphs
     return total_loss / total, correct / total
 
 def explain(model, single_loader):
@@ -144,6 +59,18 @@ def explain(model, single_loader):
             else:
                 repeat = False  # Move to next molecule
 
+def training_loop(model, loader, optimizer, criterion):
+    print("\nTraining...")
+    start_time = time.time()
+    for epoch in range(1, glob['NUM_EPOCHS'] + 1):
+        loss = train(model, loader, optimizer, criterion)
+        print(f"Epoch {epoch}: Loss {loss:.3f}  Time {time.time() - start_time:.0f}s")
+
+def evaluate(model, loader, criterion, flag):
+    print("\nEvaluating...")
+    loss, acc = test(model, loader, criterion)
+    print(f"{flag}: Loss {loss:.3f}  Acc {acc:.3f}")
+    return loss, acc
 
 def save(model):
     model_path = f"model_{time.strftime('%Y%m%d_%H%M')}_{glob['BATCH_SIZE']}_{glob['LR']}_{glob['NUM_EPOCHS']}.pt"
@@ -151,60 +78,109 @@ def save(model):
     print(f"Model saved to: {model_path}")
 
 def load(model_path):
+    print(f"\nLoading model {model_path}")
     model = MAGClassifier(ATOM_DIM, BOND_DIM).to(DEVICE)
     model.load_state_dict(torch.load(model_path, weights_only=True))
     model.eval()
     return model
 
-def main():
-    criterion = nn.BCEWithLogitsLoss()
+def crossvalidation(model, optimizer, criterion):
+    num_folds = 5
+    print(f"\nCross-Validation on: ", DATASET_PATH)
+    dataset = GraphDataset(DATASET_PATH)
+    dataset_size = len(dataset)
+    indices = torch.randperm(dataset_size).tolist()
+    fold_size = dataset_size // num_folds
+    fold_results = {
+        'test_losses': [],
+        'test_accs': [],
+    }
 
-    ## Train
-    print(f"\nTraining on: {DATASET_PATH}")
-    trainingset = GraphDataset(DATASET_PATH, split='Training')
-    loader = DataLoader(trainingset, batch_size=glob['BATCH_SIZE'], shuffle=True, drop_last=True)
+    start_time = time.time()   
+    for fold in range(num_folds):        
+        # Create indices
+        test_start = fold * fold_size
+        test_end = (fold + 1) * fold_size if fold < num_folds - 1 else dataset_size
+        test_indices = indices[test_start:test_end]
+        train_indices = indices[:test_start] + indices[test_end:]        
+        # Create subsets
+        train_subset = torch.utils.data.Subset(dataset, train_indices)
+        test_subset = torch.utils.data.Subset(dataset, test_indices)
+        train_loader = DataLoader(train_subset, batch_size=glob['BATCH_SIZE'], shuffle=True, drop_last=True)
+        test_loader = DataLoader(test_subset, batch_size=glob['BATCH_SIZE'])
+
+        print(f"\n{'='*50}\nFold {fold+1}/{num_folds}\n{'='*50}")
+        print(f"Train size: {len(train_indices)}, Test size: {len(test_indices)}")
+        training_loop(model, train_loader, optimizer, criterion)
+        loss, acc = evaluate(model, test_loader, criterion, flag=f"Fold {fold+1}")        
+        fold_results['test_losses'].append(loss)
+        fold_results['test_accs'].append(acc)    
+
+    # Print composite statistics
+    print(f"\n{'='*50}\nCROSS-VALIDATION RESULTS\n{'='*50}")    
+    mean_loss = sum(fold_results['test_losses']) / num_folds
+    std_loss = (sum((x - mean_loss)**2 for x in fold_results['test_losses']) / num_folds) ** 0.5    
+    mean_acc = sum(fold_results['test_accs']) / num_folds
+    std_acc = (sum((x - mean_acc)**2 for x in fold_results['test_accs']) / num_folds) ** 0.5    
+    print(f"Test Loss:  {mean_loss:.3f} ± {std_loss:.3f}")
+    print(f"Test Acc:   {mean_acc:.3f} ± {std_acc:.3f}")
+    print(f"\nIndividual fold accuracies: {[f'{acc:.3f}' for acc in fold_results['test_accs']]}")
+    print(F"\nTOTAL TIME: {time.time() - start_time:.0f}s")
+    print(f"{'='*50}\n")
+
+
+def main():
     model = MAGClassifier(ATOM_DIM, BOND_DIM).to(DEVICE)
     optimizer = torch.optim.AdamW(model.parameters(), lr=glob['LR'])
-    start_time = time.time()
-    for epoch in range(1, glob['NUM_EPOCHS'] + 1):
-        loss = train(model, loader, optimizer, criterion, epoch)
-        print(f"Epoch {epoch}: Loss {loss:.3f} Time {time.time() - start_time:.0f}s")
-    # save(model)
+    criterion = torch.nn.BCEWithLogitsLoss()
+
+    if CROSS_VALIDATION:
+        crossvalidation(model, optimizer, criterion)
+        return
+        
+    ## Train
+    print(f"\nTraining set: {DATASET_PATH} ('Training')")
+    trainingset = GraphDataset(DATASET_PATH, split='Training')
+    train_loader = DataLoader(trainingset, batch_size=glob['BATCH_SIZE'], shuffle=True, drop_last=True)
+    training_loop(model, train_loader, optimizer, criterion)
+    ## Statistics on Training set
     # loader = DataLoader(trainingset, batch_size=glob['BATCH_SIZE'])
-    # loss, acc  = test(model, loader, criterion)
-    # print(f"Training Loss: {loss:.3f} Acc: {acc:.3f}")
+    # statistics(model, loader, criterion, flag="Train")
 
-    # # Load saved model
-    # model_path = 'model_20250822_210138.pt'
-    # print(f"\nLoading model {model_path}")
-    # model = load(model_path)
+    ## Save model
+    # save(model)
 
-    # Test
-    print(f"\nTesting on: {DATASET_PATH}")
+    ## Load saved model
+    # model = load(MODEL_PATH)
+
+    ## Test
+    print(f"\nTest set: {DATASET_PATH} ('Test')")
     testset = GraphDataset(DATASET_PATH, split='Test')
     test_loader = DataLoader(testset, batch_size=glob['BATCH_SIZE'])
-    test_loss, test_acc = test(model, test_loader, criterion)
-    print(f"Test Loss: {test_loss:.3f} Test Acc: {test_acc:.3f}")
+    evaluate(model, test_loader, criterion, flag="Test")
 
     # # Explain
     # single_loader = DataLoader(testset, batch_size=1)
     # explain(model, single_loader)
 
 if __name__ == "__main__":
-    # Set seeds for reproducibility
+    ## Set seeds for reproducibility
     torch.manual_seed(42)
     torch.cuda.manual_seed_all(42)
-    # GLOBALS
+    ## DEBUG
     BATCH_DEBUG = None
     # BATCH_DEBUG =  True  # Debug: use batch Attention even on CPU
+    CROSS_VALIDATION = False
+    ## GLOBALS
     DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     DATASET_PATH = 'DATASETS/MUTA_SARPY_4204.csv'
+    MODEL_PATH = 'model_20250822_210138.pt'
     glob = {
         "BATCH_SIZE": 32,  # I should try reducing waste since drop_last=True
         "LR": 1e-4,
-        "NUM_EPOCHS": 1,
+        "NUM_EPOCHS": 20,
     }
-    # Print time and model stamps
+    ## Print time and model stamps
     print()
     print(time.strftime("%Y-%m-%d %H:%M:%S"))
     print(f"DEVICE: {DEVICE}")
---
+++ model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+from torch_geometric.utils import to_dense_batch
+from architectures import ESA, mlp
+from adj_mask_utils import edge_adjacency, edge_mask
+
+
+class MAGClassifier(nn.Module):
+    
+    def __init__(self, node_dim, edge_dim, hidden_dim=128, mlp_hidden_dim=128, num_heads=8, output_dim=1):
+        super(MAGClassifier, self).__init__()
+        self.hidden_dim = hidden_dim
+        # Edge feature encoder (node-edge MLP)
+        self.input_mlp = mlp(2 * node_dim + edge_dim, mlp_hidden_dim, hidden_dim)
+        # ESA block
+        self.esa = ESA(hidden_dim, num_heads, 'MMSP')
+        # Classifier
+        self.output_mlp = mlp(hidden_dim, mlp_hidden_dim, output_dim)
+
+    def batch_forward(self, edge_features, edge_index, node_batch):
+        batched_h = self.input_mlp(edge_features)  # [batch_edges, hidden_dim]
+        edge_batch = self._edge_batch(edge_index, node_batch)  # [batch_edges]
+        max_edges = torch.bincount(edge_batch).max().item()
+        dense_batch_h, pad_mask = to_dense_batch(batched_h, edge_batch, fill_value=0, max_num_nodes=max_edges)
+        batch_size = node_batch.max().item() + 1
+        adj_mask = edge_mask(edge_index, node_batch, batch_size, max_edges)
+        out = self.esa(dense_batch_h, adj_mask, pad_mask)  # [batch_size, hidden_dim]
+        # out = torch.where(pad_mask.unsqueeze(-1), out, torch.zeros_like(out))
+        logits = self.output_mlp(out)    # [batch_size, output_dim]
+        return torch.flatten(logits)     # [batch_size] 
+
+    def single_forward(self, edge_features, edge_index, node_batch, return_attention=False):
+        self.esa.expose_attention(return_attention)
+        batched_h = self.input_mlp(edge_features)  # [batch_edges, hidden_dim]
+        edge_batch = self._edge_batch(edge_index, node_batch)  # [batch_edges]
+        batch_size = node_batch.max().item() + 1
+        src, dst = edge_index
+        attn_weights = []
+        out = torch.zeros((batch_size, self.hidden_dim), device=edge_features.device)
+        for i in range(batch_size):
+            graph_mask = (edge_batch == i)
+            h = batched_h[graph_mask]  # [graph_edges, hidden_dim]
+            adj_mask = edge_adjacency(src[graph_mask], dst[graph_mask])  # [graph_edges, graph_edges]
+            h = h.unsqueeze(0)  # Add batch dimension
+            adj_mask = adj_mask.unsqueeze(0)  # Add batch dimension
+            out[i] = self.esa(h, adj_mask)  # [hidden_dim]
+            # out[i] = out[i].squeeze(0)  # Remove batch dimension ???
+            if return_attention:
+                attn = self.esa.get_attention().squeeze(0)  # Remove batch dimension
+                attn_weights.append(attn.detach().cpu())
+                # if i == batch_size - 1:
+                #     return attn_weights
+        if return_attention:
+            return attn_weights
+        logits = self.output_mlp(out)    # [batch_size, output_dim]
+        return torch.flatten(logits)     # [batch_size]
+    
+    def forward(self, batch, BATCH_DEBUG=False):
+        """
+        Args:
+            batch: batch from DataLoader (torch_geometric.data.Batch)
+                batch.x              # [batch_nodes, node_dim]
+                batch.edge_index     # [2, batch_edges]
+                batch.edge_attr      # [batch_edges, edge_dim]
+                batch.batch          # [batch_nodes]
+        """
+        edge_feat = MAGClassifier.get_features(batch)
+
+        if BATCH_DEBUG or edge_feat.device.type == 'cuda':  # GPU: batch Attention
+            return self.batch_forward(edge_feat, batch.edge_index, batch.batch)
+        else:  # per-graph Attention (faster on CPU)
+            return self.single_forward(edge_feat, batch.edge_index, batch.batch)
+        # if not torch.allclose(batch_logits, single_logits, rtol=1e-4, atol=1e-7):
+        #     print("WARNING: Batch and Single logits differ!")
+        # return batch_logits
+        
+    @staticmethod
+    def get_features(batch):
+        # Concatenate node (src and dst) and edge features
+        src, dst = batch.edge_index
+        return torch.cat([batch.x[src], batch.x[dst], batch.edge_attr], dim=1)
+
+    @staticmethod
+    def _edge_batch(edge_index, node_batch):
+        # Given edge_index [2, num_edges] and node_batch [num_nodes], 
+        # return edge_batch [num_edges] where each edge takes the batch idx of its src node
+        # (assumes all edges within a graph)
+        return node_batch[edge_index[0]]
