1c9ecb45deb482fe508358c5361a066e3863b693
totem <odioidentisti@gmail.com>
2025-11-20 22:07:18 +0100

    bug

--- output.py
+++ output.py
@@ -11,7 +11,6 @@ def cv_statistics(fold_results, task):
         best_idx = min(range(len(mean_metrics)), key=lambda i: mean_metrics[i])
     else:  # classification - higher is better
         best_idx = max(range(len(mean_metrics)), key=lambda i: mean_metrics[i])
-    best_idx = min(range(len(mean_metrics)), key=lambda i: mean_metrics[i])
     best_epoch = (best_idx + 1) * 5
 
     # Get metrics at best epoch
