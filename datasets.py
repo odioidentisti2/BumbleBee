69220888181599dfaa4b6851e00355a1f9bc5e9c
totem <odioidentisti@gmail.com>
2025-12-23 19:16:42 +0100

    {-1, +1} Hinge

--- datasets.py
+++ datasets.py
@@ -3,7 +3,7 @@ muta = {
     'path': 'DATASETS/MUTA_SARPY_4204.csv',
     'smiles_header': 'SMILES',
     'target_header': 'Experimental_value',
-    'tox_map': {'Mutagenic': 1, 'NON-Mutagenic': 0},
+    'tox_map': {'Mutagenic': 1, 'NON-Mutagenic': -1},
     # Split info (optional)
     'split_header': 'Status',
     'train_split': 'Training',
