2b652b3e7fa52dc7c60cf484ac6fe0366c1cb054
totem <odioidentisti@gmail.com>
2025-12-23 23:05:06 +0100

    test only CUBLAS

--- bumblebee.py
+++ bumblebee.py
@@ -246,8 +246,8 @@ if __name__ == "__main__":
     import os
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
     # torch.use_deterministic_algorithms(True, warn_only=True)
-    torch.backends.cuda.matmul.allow_tf32 = False
-    torch.backends.cudnn.allow_tf32 = False
+    # torch.backends.cuda.matmul.allow_tf32 = False
+    # torch.backends.cudnn.allow_tf32 = False
     # torch.use_deterministic_algorithms(True)
     ## GLOBALS
     BATCH_DEBUG =  False  # Debug: use batch Attention even on CPU
