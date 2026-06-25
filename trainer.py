11e89d039507c5b635a61f273e8992ad88fcb955
totem <odioidentisti@gmail.com>
2026-06-25 21:21:37 +0200

    bug fix

--- trainer.py
+++ trainer.py
@@ -60,7 +60,7 @@ class Trainer:
         return total_loss / total
 
     def train(self, model, loader, val_loader=None):
-        self.model.task = self.task
+        model.task = self.task
         self.optim = torch.optim.AdamW(model.parameters(), lr=PARAMS['lr'])
         max_epochs = 1  # max(1, PARAMS['max_steps'] // len(loader))
         val_interval = stopper = None
