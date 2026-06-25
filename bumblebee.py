39d64c249220f28c09e542bed16e6f7e98a0640f
totem <odioidentisti@gmail.com>
2026-06-25 21:49:12 +0200

    test load muta

--- bumblebee.py
+++ bumblebee.py
@@ -113,7 +113,7 @@ def main_loop(dataset_info, device, model_name=None):
         ### Save model
         save(model, explainer, "MODELS/muta_explainer.pt")
 
-    # else:  # Load saved model
+    else:  # Load saved model
         model_name = "muta_explainer.pt"
         model, explainer = load(f"MODELS/{model_name}", device)
 
@@ -145,7 +145,7 @@ if __name__ == "__main__":
 
     for dataset_info in _datasets:
         # model_name = 'logp.pt'
-        # model_name = 'muta.pt'
+        model_name = 'muta_explainer.pt'
 
         ### Reproducibility  (MSELoss => regression is deterministic enough ?)
         if dataset_info['task'] == 'binary_classification':
