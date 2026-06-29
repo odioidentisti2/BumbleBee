1091efc413c5a29c7caf5cd256dfd88530bbbbe9
totem <odioidentisti@gmail.com>
2026-06-29 21:00:29 +0200

    cleaned

--- bumblebee.py
+++ bumblebee.py
@@ -83,10 +83,6 @@ def main_loop(dataset_info, device, model_name=None):
         print("\nTraining...")
         calibration_data = trainer.train(model, trainingset, validation_set)
 
-        model = MAG(ATOM_DIM, BOND_DIM)
-        trainer.train(model, trainingset, validation_set)
-
-
         ## Statistics on Training set
         # print("\nEvaluating on training set...")
         # trainer.eval(model, trainingset, flag="Train")  # WHY IS THIS SO DIFFERENT FROM TRAINING LOSS?
