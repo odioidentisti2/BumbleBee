1fa3c324525140546aac6f47c0da8c0036fcd29d
totem <odioidentisti@gmail.com>
2026-07-07 15:46:41 +0000

    muta benchmark

--- bumblebee.py
+++ bumblebee.py
@@ -117,8 +117,8 @@ if __name__ == "__main__":
 
     model_name = None
     _datasets = []
-    _datasets.append(datasets.logp_split)
-    # _datasets.append(datasets.muta)
+    # _datasets.append(datasets.logp_split)
+    _datasets.append(datasets.muta)
 
     for dataset_info in _datasets:
         # model_name = 'LOGP_new_inj.pt'
